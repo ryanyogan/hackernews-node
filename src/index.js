a6d5957fb5fa9b606496e1598457c304cfc5e66e
Ryan Yogan <ryanyogan@gmail.com>
2018-05-20 15:49:50 -0700

    Toying around with Prisma, going to use this for class

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { Prisma } = require("prisma-binding");
+
+const resolvers = {
+  Query: {
+    info: () => "This is the API of the Hackernews Clone",
+    feed: (root, args, ctx, info) => ctx.db.query.links({}, info)
+  },
+  Mutation: {
+    post: (root, args, ctx, info) =>
+      ctx.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description
+          }
+        },
+        info
+      )
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: "./database/src/generated/prisma.graphql",
+      endpoint: "https://us1.prisma.sh/public-jetstork-958/hackernews-node/dev",
+      secret: "mysecret123",
+      debug: true
+    })
+  })
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
