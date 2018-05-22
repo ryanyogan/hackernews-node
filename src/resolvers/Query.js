8659b5869ca02dc73254755cf74f3ac5d0f85c5e
Ryan Yogan <ryanyogan@gmail.com>
2018-05-21 17:23:49 -0700

    added in pagination of feed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,15 @@
-const feed = (parent, args, context, info) => context.db.query.links({}, info);
+const feed = (parent, args, context, info) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { url_contains: args.filter },
+          { description_contains: args.filter }
+        ]
+      }
+    : {};
+
+  return context.db.query.links({ where }, info);
+};
 
 module.exports = {
   feed
